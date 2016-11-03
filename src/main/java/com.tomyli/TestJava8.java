a7675a784534a46357bb9e79d98a24d43273e484
peng051410 <peng051410@126.com>
2016-11-03 09:54:40 +0800

    Initial commit

---
+++ src/main/java/com.tomyli/TestJava8.java
@@ -0,0 +1,13 @@
+package com.tomyli;
+
+/**
+ * lizp@ishugui.com
+ *
+ * @author tomyli 2016/11/03 09:53.
+ */
+public class TestJava8 {
+
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
