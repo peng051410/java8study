8c3ac05f58789471d07f0a405ee40dbbe6c004cd
peng051410 <peng051410@126.com>
2020-01-07 11:20:21 +0800

    Up

--- src/main/java/cn/imcompany/leetcode/No001TwoSum.java
+++ src/main/java/cn/imcompany/leetcode/No001TwoSum.java
@@ -1,5 +1,9 @@
 package cn.imcompany.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 给一个int数组，返回数组中两个数字相加的和是目标数的下标。
  * 可以假设每个输入只有一个解决方案，并且不能使用同一个元素两次。
@@ -13,5 +17,19 @@ public class No001TwoSum {
 
     public static void main(String[] args) {
 
+        System.out.println(Arrays.toString(sum2(new int[]{2, 7, 11, 15}, 9)));
+    }
+
+    public static int[] sum2(int[] arr, int target) {
+
+        Map<Integer, Integer> map = new HashMap<>(arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            int second = target - arr[i];
+            if (map.containsKey(second)) {
+                return new int[]{map.get(second), i};
+            }
+            map.put(arr[i], i);
+        }
+        return new int[]{};
     }
 }
